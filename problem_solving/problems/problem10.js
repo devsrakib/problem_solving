f4b3eaa2fffc9e0460e85861ab391d51df38e86c
devsrakib <devs.rakibulislam@gmail.com>
2024-10-09 10:45:04 +0600

    created a function that return an object with largest string and index number

--- problem_solving/problems/problem10.js
+++ problem_solving/problems/problem10.js
@@ -4,5 +4,64 @@ const sentence = 'ami muhammad rakibul islam shahin, amar baba amake shahin bole
 const matches = sentence.match(/shahin/gi);
 const occurances = matches ? matches.length : 0
 const position = sentence.search(/shahin/i);
-console.log(position);
+// console.log(position);
+
+// linear search
+const search = (arr, val) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === val) {
+            return i
+        }
+    }
+}
+
+const result = search(['a', 'c', 'd', 'e', 'f', 'g'], 'c');
+// console.log(result);
+
+
+
+// get the largest string from an array
+const getString = (arr) => {
+    let string = ''
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].length > string.length) {
+            string = arr[i]
+        }
+    }
+    return {
+        string: string,
+        index: arr.indexOf(string)
+    };
+
+}
+
+const result1 = getString(['rakib', 'sakib', 'muntasir', 'forTesting'])
+console.log(result1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
